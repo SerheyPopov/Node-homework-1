5ded9b4d5de9623d83e77b531f6d8cb9fbccef66
SerheyPopov <serheypopov1994@gmail.com>
2023-07-05 17:07:53 +0300

    node-homework-1-part-1

---
+++ README.md
@@ -0,0 +1,7 @@
+https://monosnap.com/file/JgKoU8PtbXRwFONBFTrUcfeyPLrEFS
+
+https://monosnap.com/file/f06jfnPGYs0G27jFeHzpTGYLi6vO2H
+
+https://monosnap.com/file/g9J2uyThOupl5GRgrVR9mEAaHiDEc7
+
+https://monosnap.com/file/Ku4dwvrAVeshvSly1vEhRwyOtWBRXX
---
+++ index.js
@@ -0,0 +1,32 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
+program.parse(process.argv);
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+	switch (action) {
+		case "list":
+			const allContacts = await contacts.listContacts();
+			return console.table(allContacts);
+		case "get":
+			const contact = await contacts.getContactById(id);
+			return console.table(contact);
+		case "add":
+			const newContact = await contacts.addContact({ name, email, phone });
+			return console.table(newContact);
+
+		case "remove":
+			const remove = await contacts.removeContact(id);
+			return console.table(remove);
+	}
+};
+
+invokeAction(argv);
